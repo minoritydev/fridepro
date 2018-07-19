086c69a76e227d8bd2834a661f8b55230a06216c
minoritydev <30841345+minoritydev@users.noreply.github.com>
2018-07-19 21:28:11 +0530

    fixed NoneTypeObject error

--- ui.py
+++ ui.py
@@ -1,13 +1,15 @@
 from tkinter import *
 window = Tk()
-frame = Frame(window).place(x=250,y=250)
+frame = Frame(window, width=500, height=500)
+frame.place(x=1,y=1)
 #fucntions
+
 def onClickFill():
+   
+    frame.destroy()
+    fillFrame = Frame(window).place(x=250,y=250)
     
-    self.frame.destroy()
-    fillFrame = Frame(window)
-    
-    window.title("Welcome to LikeGeeks app")
+    window.title("test1")
    
     vegSearchLabel = Label(fillFrame, text="Please enter name of product:", font=("Helvetica", 22)).place(x=0,y=0)
     vegSearchEntry = Entry(fillFrame, width=50).place(x=0, y=60)
@@ -15,9 +17,10 @@ def onClickFill():
     
 #main window
 window.geometry("500x500")
-window.title("Welcome to LikeGeeks app")
+window.title("test")
 fillBtn = Button(frame, text="FILL", height=17, width=60, bg="#75d179", command=onClickFill).place(x=0, y=0)
 takeBtn = Button(frame, text="TAKE", height=17, width=60, bg="blue").place(x=0,y=250)
+
 #main window over
 
 
