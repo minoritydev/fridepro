fb996a29ad832fe63c634eeefe3b9abdcbb48606
minoritydev <30841345+minoritydev@users.noreply.github.com>
2018-07-21 15:41:59 +0530

    fixed bug with destroy() on Back

--- ui.py
+++ ui.py
@@ -1,28 +1,35 @@
 from tkinter import *
+import pymysql
+#database
+db =  pymysql.connect(
+    host='localhost',
+    user='root',
+    passwd='1337',
+    db='fridepro'
+)
+
+
 window = Tk()
-frame = Frame(window, width=500, height=500)
-frame.place(x=1,y=1)
 #fucntions
 
 def onClickFill():
    
-    frame.destroy()
-    fillFrame = Frame(window).place(x=250,y=250)
-    
-    window.title("test1")
-   
-    vegSearchLabel = Label(fillFrame, text="Please enter name of product:", font=("Helvetica", 22)).place(x=0,y=0)
-    vegSearchEntry = Entry(fillFrame, width=50).place(x=0, y=60)
+    windowFill= Tk()
+    windowFill.title("test1")
+    windowFill.geometry("500x500")
+    vegSearchLabel = Label(windowFill, text="What are item(s) are you putting in?", font=("Helvetica", 22)).place(x=0,y=0)
+    vegSearchEntry = Entry(windowFill, width=50, font=("Helvetica", 20)).place(x=0, y=60)
+    fillBackBtn= Button(windowFill, text="Back", command=windowFill.destroy).place(x=0, y=450)
 #function over
-    
+
 #main window
 window.geometry("500x500")
 window.title("test")
-fillBtn = Button(frame, text="FILL", height=17, width=60, bg="#75d179", command=onClickFill).place(x=0, y=0)
-takeBtn = Button(frame, text="TAKE", height=17, width=60, bg="blue").place(x=0,y=250)
+fillLbl = Label(window,text="Are you -", font=("Helvetica",10)).place(x=0,y=0)
+fillBtn = Button(window, text="Filling in?", height=15, width=60, bg="#75d179", command=onClickFill,).place(x=0, y=20)
+takeBtn = Button(window, text="Taking out?", height=17, width=60, bg="red", fg="white").place(x=0,y=250)
 
 #main window over
-
 
 
 window.mainloop()
