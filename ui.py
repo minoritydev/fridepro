8a7ed40c59811df52058dbb3eff34dfd5f5b2019
minoritydev <30841345+minoritydev@users.noreply.github.com>
2018-07-25 23:35:06 +0530

    added new window to enter qty

--- ui.py
+++ ui.py
@@ -7,11 +7,12 @@ db =  pymysql.connect(
     passwd='1337',
     db='fridepro'
 )
-
+cursor=db.cursor()
 
 window = Tk()
 
 valSearch=StringVar()
+valQty=IntVar()
 #fucntions
 
     
@@ -23,15 +24,51 @@ def onClickFill():
     vegSearchLabel = Label(windowFill, text="What are item(s) are you putting in?", font=("Helvetica", 22)).place(x=0,y=0)
     vegSearchEntry = Entry(windowFill, width=50, font=("Helvetica", 20), )
     vegSearchEntry.place(x=0, y=60)
-    def temp():
+    
+    #getNameFromDB will fetch entered name from DB (if it exsists) 
+    def getNameFromDB():
         valSearch=vegSearchEntry.get()
-        print(valSearch)
+        #print(valSearch)
+        
+        def quantityWindow(): #opens new window to enter quantity of item
+            cursor.execute("select quantity from vegetables where name=%s",valSearch)
+            quantity = cursor.fetchall()
+            windowQty= Tk()
+            windowQty.title("Enter Amount to Fill")
+            windowQty.geometry("500x500")
+            vegQtyInputLabel = Label(windowQty, text=("How many "+valSearch+" will you put in?"), font=("Helvetica", 22)).place(x=0,y=0)
+            vegPresentQtyLabel = Label(windowQty, text=(valSearch+" currently stored:", quantity), font=("Helvetica", 15)).place(x=0,y=60)
+            vegSearchEntry = Entry(windowQty, width=50, font=("Helvetica", 20)).place(x=0, y=120)
+            button0=Button(windowQty, text="0").place(x=200, y=320)
+            button1=Button(windowQty, text="1").place(x=160, y=200)
+            button2=Button(windowQty, text="2").place(x=200, y=200)
+            button3=Button(windowQty, text="3").place(x=240, y=200)
+            button4=Button(windowQty, text="4").place(x=160, y=240)
+            button5=Button(windowQty, text="5").place(x=200, y=240)
+            button6=Button(windowQty, text="6").place(x=240, y=240)
+            button7=Button(windowQty, text="7").place(x=160, y=280)
+            button8=Button(windowQty, text="8").place(x=200, y=280)
+            button9=Button(windowQty, text="9").place(x=240, y=280)
+            fillBackBtn= Button(windowQty, text="Back", command=windowQty.destroy).place(x=0, y=450)
+        #end of quantityWindow()
+            
+        cursor.execute("select * from vegetables where name=%s",valSearch)
+        affectedRow = cursor.rowcount
+        data = cursor.fetchall()
+        if affectedRow == 0:
+            itemNotFoundLbl=Label(windowFill,text=(valSearch+" is not present in database,\n would you like to add it?"),font=("Helvetica", 18)).place(x=20,y=150)
+            addYesBtn=Button(windowFill, text="Yes", font=("Helvetica",15), bg="#75d179").place(x=100,y=220)
+            addNoBtn=Button(windowFill, text="No", font=("Helvetica",15), bg= "red", fg="white").place(x=170,y=220)
+        else:
+            print(data)
+       
+            quantityWindow()
+     #getNameFromDB ends here
+            
     fillBackBtn= Button(windowFill, text="Back", command=windowFill.destroy).place(x=0, y=450)
-    fillOkBtn= Button(windowFill, text="Ok", command=temp).place(x=450, y=450)
+    fillOkBtn= Button(windowFill, text="Ok", command=getNameFromDB).place(x=450, y=450)
     print("ok")
 
-    
-#function over
 def onClickTake():
    
     windowFill= Tk()
